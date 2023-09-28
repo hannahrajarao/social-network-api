94cd405dc3a19daf586eee41661954bc2952cc18
hannahrajarao <hannahrajarao@gmail.com>
2023-09-28 18:32:54 -0400

    Update user

--- controllers/userController.js
+++ controllers/userController.js
@@ -34,6 +34,25 @@ module.exports = {
       res.status(500).json(err);
     }
   },
+  //update a user
+  async updateUser(req, res) {
+    try {
+      const user = await User.findOneAndUpdate(
+        { _id: req.params.userId },
+        { $set: req.body },
+        { runValidators: true, new: true }
+      );
+      
+      if (!user) {
+        return res.status(404).json({ message: 'No user with that ID' });
+      }
+
+      res.json(user);
+    }
+    catch (err) {
+      res.status(500).json(err);
+    }
+  },
   // Delete a user and associated apps
   async deleteUser(req, res) {
     try {
